504a9cc299eb229ba871ba620aeca5657a696feb
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2021-04-10 14:17:23 +0200

    Fix pipeline to run privileged commands

--- .github/workflows/scripts/install_kcov.sh
+++ .github/workflows/scripts/install_kcov.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -xeu
 
-apt-get update
-apt-get install zlib1g-dev libelf-dev libcurl4-openssl-dev libdw-dev libbfd-dev libiberty-dev
+sudo apt-get update
+sudo apt-get install zlib1g-dev libelf-dev libcurl4-openssl-dev libdw-dev libbfd-dev libiberty-dev
 wget https://github.com/SimonKagstrom/kcov/archive/"$KCOV_VERSION".tar.gz
 tar xzf "$KCOV_VERSION".tar.gz
 cd kcov-"$KCOV_VERSION"
