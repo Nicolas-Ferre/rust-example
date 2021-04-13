4d33220c4efabc293c39b47740833fc77ddc3607
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2021-04-13 22:23:22 +0200

    Echo message when coverage is not uploaded on Codecov

--- .github/workflows/scripts/upload_coverage_on_codecov.sh
+++ .github/workflows/scripts/upload_coverage_on_codecov.sh
@@ -4,4 +4,6 @@ set -xeu
 if [ -n "$CODECOV_TOKEN" ]; then
     grcov . --binary-path ./target/debug/ -s . -t lcov --branch --ignore-not-existing -o ./lcov.info --excl-start "#\[cfg\(test\)\]" --keep-only **/src/**/*
     bash <(curl -s https://codecov.io/bash) -f lcov.info
+else
+    echo "Coverage not uploaded on Codecov because CODECOV_TOKEN environment variable is not set"
 fi
