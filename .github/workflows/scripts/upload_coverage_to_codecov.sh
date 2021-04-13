ce1f3c4e856ad75a819ce31d86a514a0c3c5c946
Nicolas Ferré <nicolas.ferre.public@gmail.com>
2021-04-13 22:15:40 +0200

    Fix workflow

--- .github/workflows/scripts/upload_coverage_to_codecov.sh
+++ .github/workflows/scripts/upload_coverage_to_codecov.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -xeu
 
-grcov . --binary-path ./target/debug/ -s . -t lcov --branch --ignore-not-existing -o ./lcov.info --excl-start "#\[cfg\(test\)\]" --keep-only **/src/**/*
-bash <(curl -s https://codecov.io/bash) -f lcov.info
+if [ -n "$CODECOV_TOKEN" ]; then
+    grcov . --binary-path ./target/debug/ -s . -t lcov --branch --ignore-not-existing -o ./lcov.info --excl-start "#\[cfg\(test\)\]" --keep-only **/src/**/*
+    bash <(curl -s https://codecov.io/bash) -f lcov.info
+fi
